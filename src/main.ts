1a68ad144c56030ad47fe3eb76b11f91fb31af49
brilaja <bril@gmail.com>
2024-07-01 15:53:27 +0800

    test

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,9 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Router } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const router = Router();
-
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
